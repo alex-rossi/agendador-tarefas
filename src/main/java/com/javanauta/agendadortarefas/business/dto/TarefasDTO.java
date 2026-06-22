a6431c8ed40aa98d1371c540eabc28f43d72cb88
Alex <alexx.rf.18@gmail.com>
2026-06-21 21:16:11 -0300

    adicionados métodos GET por email e por período

--- src/main/java/com/javanauta/agendadortarefas/business/dto/TarefasDTO.java
+++ src/main/java/com/javanauta/agendadortarefas/business/dto/TarefasDTO.java
@@ -16,10 +16,12 @@ public class TarefasDTO {
     private String id;
     private String nomeTarefa;
     private String descricao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime dataCriacao; //serve pra trabalhar com data e hora (se fosse só com data é LocalDate)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime dataEvento;
     private String emailUsuario;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime dataAlteracao;
     private StatusNotificacaoEnum statusNotificacaoEnum;
 }
